c863c2c082ccbd5599060f4b265f9e4dd0a63e70
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-10-02 21:00:20 -0400

    Update quiz1.c

--- quiz1/new/quiz1.c
+++ quiz1/new/quiz1.c
@@ -9,6 +9,12 @@ int digit_sum_base(int z, int bas3) {
     zf = n%bas3;
     printf("answer is %d.",zf);
     x = x+1;
+  elif (z == 0):
+    digit_sum_base(z/bas3);
+    zf = n%bas3;
+    int xc;
+    xc = 0;
+    printf("answer is %d.",xc);
     
     
   return zf;
